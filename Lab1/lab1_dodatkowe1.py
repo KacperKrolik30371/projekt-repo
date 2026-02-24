d9c871df4ad4ba1086de675f93d70193a211e9a4
”Kacper” <30371@student.ajp.edu.pl>
2026-02-24 14:10:21 +0100

    .

---
+++ Lab1/lab1_dodatkowe1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("sales_raw.csv")
+
+df["total_value"] = df["quantity"] * df["unit_price"]
+
+sales_by_country = df.groupby("country")["total_value"].sum()
+sales_by_product = df.groupby("product_name")["total_value"].sum()
+
+print(sales_by_country)
+print(sales_by_product)
+
+df_high_value = df[df["total_value"] > 1000]
+
+df_high_value.to_csv("high_value_sales.csv", index=False)
+
+transactions_by_country = df_high_value.groupby("country").size()
+
+print(transactions_by_country)
